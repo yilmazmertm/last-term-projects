bca60de8becf4744dd0088aa88b1a74a4051aebf
yilmazmertm <yilmazmertm@gmail.com>
2020-08-26 14:02:50 +0100

    adding websocket project

---
+++ websocket-01/src/test/java/com/yilmazmertm/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yilmazmertm.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
